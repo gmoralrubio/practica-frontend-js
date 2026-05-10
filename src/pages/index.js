fc808812827efb5f2f53a129d51d98b95d360d29
gmoralrubio <gmoralrubio@gmail.com>
2026-05-10 18:21:52 +0200

    fix: añadido checkeo del formulario de nuevo producto antes de modificar el classList

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,12 +41,12 @@ newProductActionContainer.addEventListener('newProductBtnClicked', () =>
 newProductActionContainer.addEventListener('closeModalBtnClicked', () => modal.close())
 newProductActionContainer.addEventListener('productCreationStarted', () => {
 	const form = modal.querySelector('.new-product-form')
-	form.classList.add('hidden')
+	form?.classList.add('hidden')
 	showLoader(modalLoaderContainer)
 })
 newProductActionContainer.addEventListener('productCreationEnded', () => {
 	const form = modal.querySelector('.new-product-form')
-	form.classList.remove('hidden')
+	form?.classList.remove('hidden')
 	hideLoader(modalLoaderContainer)
 })
 newProductActionContainer.addEventListener('productCreationFailed', (e) =>
